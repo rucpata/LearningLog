99eb71ce27617a9a6b259fc850bc2d478d23f74a
rucpata <rucinska.patrycja@gmail.com>
2020-09-25 20:20:14 +0200

    New class Entry

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -9,3 +9,16 @@ class Topic(models.Model):
     def __str__(self):
         """Zwraca reprezentację modelu w postaci ciągu tekstowego."""
         return self.text
+
+class Entry(models.Model):
+    """Konkretnre inforamcje o postępie w nauce"""
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        """Zwraca reprezentuację modelu w postaci ciągu tekstopwego."""
+        return self.text[:50] + '...'
