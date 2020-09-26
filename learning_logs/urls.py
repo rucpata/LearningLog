30176d115058382f0b77dc4cb39763e14b31340a
rucpata <rucinska.patrycja@gmail.com>
2020-09-26 22:20:27 +0200

    template topics

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -2,9 +2,11 @@
 
 from django.urls import path
 
-from .views import index
+from .views import index, topics
 
 urlpatterns = [
     #Strona główna
     path('', index, name='index'),
+    #Strona tematów
+    path('topics/', topics, name='topics'),
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 
+from .models import Topic
+
 # Create your views here.
 def index(request):
     """Strona główna aplikacji Learning Log."""
     return render(request, 'learning_logs/index.html')
+
+def topics(request):
+    """Wyświetlenie wszystkich tematów"""
+    topics = Topic.objects.order_by('data_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
