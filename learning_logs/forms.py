b00f80792e5572909bea7586d7c55979a61065d4
rucpata <rucinska.patrycja@gmail.com>
2020-09-27 20:36:57 +0200

    add forms

---
+++ learning_logs/forms.py
@@ -0,0 +1,10 @@
+import django import forms
+
+from .models import Topic
+
+class TopicForm(forms.ModelForm):
+    
+    class Meta:
+        model = Topic
+        fields = ['text']
+        labels = ['text': '']
