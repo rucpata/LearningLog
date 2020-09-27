983473b0bf83cbccd7c2cc04a1cef47a3a8ba1a4
rucpata <rucinska.patrycja@gmail.com>
2020-09-27 20:49:33 +0200

    views new_topic

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect #Przekierowanie uzytkownika po dodaniu nowego tematu do strony topic
+from django.core.urlresolvers import reverse 
 
 from .models import Topic
+from .models import TopicForm
 
-# Create your views here.
 def index(request):
     """Strona główna aplikacji Learning Log."""
     return render(request, 'learning_logs/index.html')
@@ -19,3 +21,18 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
+
+def new_topic(request):
+    """Dodaj nowy temat"""
+    if request.method != 'POST':
+        #Nie przekazano zadnych danych nalezy utowrzyc pusty formularz
+        form = TopicForm()
+    else:
+        #Przekazano dane za pomoca zadania POST, nalezy je przetworzyc
+        form = TopicForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('topcs'))
+
+        context = {'form': form}
+        return render(request, 'new_topic.html', context)
