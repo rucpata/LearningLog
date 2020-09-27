0dc106cd43005f9fde1a8dc5ebe2c1010452d6ef
rucpata <rucinska.patrycja@gmail.com>
2020-09-27 20:38:45 +0200

    url new_topic

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -11,4 +11,6 @@ urlpatterns = [
     path('topics/', topics, name='topics'),
     #Strona szczegółowa dotycząca pojedyńczego tematu
     path('topics/<topic_id>', topic, name='topic'),
+    #Strona przeznaczona do dodawania nowego tematu
+    path('new_topic/', new_topic, name='new_topic'),
 ]
