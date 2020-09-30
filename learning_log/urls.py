fade2873361ae64206990984fcd8cf123ff2127f
rucpata <rucinska.patrycja@gmail.com>
2020-09-30 13:05:40 +0200

    including URLs from users app

--- learning_log/urls.py
+++ learning_log/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('learning_logs.urls')),
+    path('users/'), include('users.urls', namespace='users')),
+    path('', include('learning_logs.urls', namespace='learning_logs')),
 ]
