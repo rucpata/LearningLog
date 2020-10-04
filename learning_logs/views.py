e94acf19fd8265155bce07b3cb0fddbba1cdfdea
rucpata <rucinska.patrycja@gmail.com>
2020-10-04 10:52:57 +0200

    protection of edit_entry

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -72,6 +72,8 @@ def edit_entry(request, entry_id):
     """Edycja istniejącego wpisu"""
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
+    if topic.owner != request.user:
+        raise Http404
 
     if request.method != 'POST':
         #Żądanie początkowe wypełnienie formularza aktualną treścią wpisu
